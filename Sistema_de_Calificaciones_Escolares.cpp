cf22248f3327fa4155e6af6051c367dbc7efb14d
ripthem <riptted@gmail.com>
2026-09-10 20:11:14 -0700

    Add files via upload
    Nivel 2. 
       - Se agrega if-else para determinar el estado del estudiante después de calcular el promedio.
       - Se agrega un if para validar el limite de edad. 
       - Se agrega un if para validar calificaciones, en caso de no cumplir arrojar un error y terminar.

--- Sistema_de_Calificaciones_Escolares.cpp
+++ Sistema_de_Calificaciones_Escolares.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+// Nivel 1
+
 int main() {
     string nombre;
     int edad;
@@ -16,6 +18,11 @@ int main() {
     cout << "Ingrese la edad: ";
     cin >> edad;
 
+    if (edad < 0 || edad > 120) {
+        cout << "Error: Edad invalida. " << endl;
+        return 1;
+    }
+
     cout << "Ingrese la primera calificacion: ";
     cin >> calificacion1;
 
@@ -25,13 +32,36 @@ int main() {
     cout << "Ingrese la tercera calificacion: ";
     cin >> calificacion3;
 
-    promedio= (calificacion1 + calificacion2 + calificacion3) / 3;
+    // Nivel 2
+
+    if (calificacion1 < 0 || calificacion1 > 10 ||
+        calificacion2 < 0 || calificacion2 > 10 ||
+        calificacion3 < 0 || calificacion3 > 10) {
+        cout << "Error: Calificaciones invalidas." << endl;
+        return 1;
+    }
+
+    promedio = (calificacion1 + calificacion2 + calificacion3) / 3.0;
 
     cout << "RESUMEN DEL ESTUDIANTE" << endl;
     cout << "Nombre: " << nombre << endl;
     cout << "Edad: " << edad << endl;
     cout << "Calificaciones: " << calificacion1 << ", " << calificacion2 << ", " << calificacion3 << endl;
-    cout << "Promedio final: " << promedio << endl;
+    cout << "Promedio: " << promedio << endl;
+
+    cout << "Estado: ";
+    if (promedio >= 9.0) {
+        cout << "EXCELENTE" << endl;
+    } 
+    else if (promedio >= 7.0) {
+        cout << "APROBADO" << endl;
+    } 
+    else if (promedio >= 6.0) {
+        cout << "REGULAR" << endl;
+    } 
+    else {
+        cout << "REPROBADO" << endl;
+    }
 
     return 0;
 }
