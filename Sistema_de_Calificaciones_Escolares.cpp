1a359e5ce95125c8476dd430b1a3078562cebed5
ripthem <riptted@gmail.com>
2026-09-16 21:21:26 -0700

    Add files via upload
    Nivel 3: switch-case
    
    - Se genera  este menú con opciones para saber que acción se debe realizar.
    === SISTEMA DE CALIFICACIONES ===
    1. Registrar estudiante
    2. Ver informacion del programa
    3. Salir
    Opción:
    
    - Se agrega un switch-case el cual nos da la opcion de tener diferentes casos.

--- Sistema_de_Calificaciones_Escolares.cpp
+++ Sistema_de_Calificaciones_Escolares.cpp
@@ -1,67 +1,89 @@
 #include <iostream>
 #include <string>
+#include <limits>
 
 using namespace std;
 
-// Nivel 1
-
 int main() {
-    string nombre;
-    int edad;
-    float calificacion1, calificacion2, calificacion3;
-    float promedio;
+    int opcion;
 
-    cout << "Registro de Estudiante" << endl;
-    cout << "Ingrese el nombre del estudiante: ";
-    getline(cin, nombre);
+    cout << "=== SISTEMA DE CALIFICACIONES ==="<< endl;
+    cout << "1. Registrar estudiante." << endl;
+    cout << "2. Ver informacion del programa." << endl;
+    cout << "3. Salir." << endl;
+    cout << "Opcion: ";
+    cin >> opcion;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    cout << "Ingrese la edad: ";
-    cin >> edad;
 
-    if (edad < 0 || edad > 120) {
-        cout << "Error: Edad invalida. " << endl;
-        return 1;
+    switch (opcion) {
+        case 1: {
+        string nombre;
+        int edad;
+        float calificacion1, calificacion2, calificacion3;
+        float promedio;
+
+        cout << "Registro de Estudiante" << endl;
+        cout << "Ingrese el nombre del estudiante: ";
+        getline(cin, nombre);
+
+        cout << "Ingrese la edad: ";
+        cin >> edad;
+
+        if (edad < 0 || edad > 120) {
+            cout << "Error: Edad invalida. " << endl;
+            return 1;
+        }
+
+        cout << "Ingrese la primera calificacion: ";
+        cin >> calificacion1;
+
+        cout << "Ingrese la segunda calificacion: ";
+        cin >> calificacion2;
+
+        cout << "Ingrese la tercera calificacion: ";
+        cin >> calificacion3;
+
+
+        if (calificacion1 < 0 || calificacion1 > 10 ||
+            calificacion2 < 0 || calificacion2 > 10 ||
+            calificacion3 < 0 || calificacion3 > 10) {
+            cout << "Error: Calificaciones invalidas." << endl;
+            return 1;
+        }
+
+        promedio = (calificacion1 + calificacion2 + calificacion3) / 3.0;
+
+        cout << "RESUMEN DEL ESTUDIANTE" << endl;
+        cout << "Nombre: " << nombre << endl;
+        cout << "Edad: " << edad << endl;
+        cout << "Calificaciones: " << calificacion1 << ", " << calificacion2 << ", " << calificacion3 << endl;
+        cout << "Promedio: " << promedio << endl;
+
+        cout << "Estado: ";
+        if (promedio >= 9.0) {
+            cout << "EXCELENTE" << endl;
+        } 
+        else if (promedio >= 7.0) {
+            cout << "APROBADO" << endl;
+        } 
+        else if (promedio >= 6.0) {
+            cout << "REGULAR" << endl;
+        } 
+        else {
+            cout << "REPROBADO" << endl;
+        }
+        break;
     }
 
-    cout << "Ingrese la primera calificacion: ";
-    cin >> calificacion1;
+    case 2:
+        cout << "Ver informacion del programa." << endl;
+        break;
 
-    cout << "Ingrese la segunda calificacion: ";
-    cin >> calificacion2;
-
-    cout << "Ingrese la tercera calificacion: ";
-    cin >> calificacion3;
-
-    // Nivel 2
-
-    if (calificacion1 < 0 || calificacion1 > 10 ||
-        calificacion2 < 0 || calificacion2 > 10 ||
-        calificacion3 < 0 || calificacion3 > 10) {
-        cout << "Error: Calificaciones invalidas." << endl;
-        return 1;
-    }
-
-    promedio = (calificacion1 + calificacion2 + calificacion3) / 3.0;
-
-    cout << "RESUMEN DEL ESTUDIANTE" << endl;
-    cout << "Nombre: " << nombre << endl;
-    cout << "Edad: " << edad << endl;
-    cout << "Calificaciones: " << calificacion1 << ", " << calificacion2 << ", " << calificacion3 << endl;
-    cout << "Promedio: " << promedio << endl;
-
-    cout << "Estado: ";
-    if (promedio >= 9.0) {
-        cout << "EXCELENTE" << endl;
+    case 3: 
+        cout << "Salir." << endl;
+        return 0;
     } 
-    else if (promedio >= 7.0) {
-        cout << "APROBADO" << endl;
-    } 
-    else if (promedio >= 6.0) {
-        cout << "REGULAR" << endl;
-    } 
-    else {
-        cout << "REPROBADO" << endl;
-    }
 
     return 0;
-}
+} 
