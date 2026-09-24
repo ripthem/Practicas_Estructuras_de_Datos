70871e911c6790f2a76c91aded9fcdc86c002ec0
ripthem <riptted@gmail.com>
2026-09-23 17:43:05 -0700

    Nivel 6
    NIVEL 6 — Ciclo do-while
    
       - Se agrego la opcion default que hacia falta del switch-case. 
       - Se agrega el ciclo do-while complementando la parte del ciclo while. 
       - Ahora despues de seleccionar la opcion 1 y 2, se puede volver a elegir otra opcion, esto no sucede con la opcion 3, cuando la opcion 3 se elige el programa se termina. 
    
    (Hay un detalle que no pude solucionar, al elegir una opción se salta un renglon y hay que volver a escribir el numero de la opción elegida).

--- Sistema_de_Calificaciones_Escolares.cpp
+++ Sistema_de_Calificaciones_Escolares.cpp
@@ -7,115 +7,129 @@ using namespace std;
 int main() {
     int opcion;
 
-    cout << "=== SISTEMA DE CALIFICACIONES ==="<< endl;
-    cout << "1. Registrar estudiante." << endl;
-    cout << "2. Ver informacion del programa." << endl;
-    cout << "3. Salir." << endl;
-    cout << "Opcion: ";
-    cin >> opcion;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    do {
+
+        cout << "=== SISTEMA DE CALIFICACIONES ==="<< endl;
+        cout << "1. Registrar estudiante." << endl;
+        cout << "2. Ver informacion del programa." << endl;
+        cout << "3. Salir." << endl;
+        cout << "Opcion: ";
+        cin >> opcion;
+
+        if (!(cin >> opcion)) {
+            cin.clear();
+            opcion = 0;        
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
 
-    switch (opcion) {
-        case 1: {
-        string nombre;
-        int edad;
-        int n;
-        float calificacion;
-        float suma = 0; 
-        float promedio;
-        int aprobadas = 0;
-        int reprobadas = 0;
-        float mayor = 10, menor = 0; 
+        switch (opcion) {
+            case 1: {
+            string nombre;
+            int edad;
+            int n;
+            float calificacion;
+            float suma = 0; 
+            float promedio;
+            int aprobadas = 0;
+            int reprobadas = 0;
+            float mayor = 10, menor = 0; 
         
 
-        cout << "Registro de Estudiante" << endl;
-        cout << "Ingrese el nombre del estudiante: ";
-        getline(cin, nombre);
+            cout << "Registro de Estudiante" << endl;
+            cout << "Ingrese el nombre del estudiante: ";
+            getline(cin, nombre);
 
-            while (true) {
-                cout << "Ingrese la edad: ";
-                if (cin >> edad && edad >= 0 && edad <= 120) {
-                    break;
+                while (true) {
+                    cout << "Ingrese la edad: ";
+                    if (cin >> edad && edad >= 0 && edad <= 120) {
+                        break;
+                    }
+                cout << "Error: Edad invalida, favor de ingresar un numero entre 0 y 120." << endl;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                }    
+
+                while (true) {
+                    cout << "Cuantas calificaciones desea registrar (1-3)? ";
+                    if (cin >> n && n >= 1 && n <= 3) {
+                        break;
+                    }
+                    cout << "Error: Solo se pueden registrar de 1 a 3 calificaciones." << endl;
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                
+                }
+
+                for (int i = 1; i <= n; i++) {
+                    cout << "Ingrese la calificacion " << i << ": ";
+                    cin >> calificacion;
+
+                    if (calificacion < 0 || calificacion > 10) {
+                        cout << "Error: Calificaciones invalidas." << endl;
+                        return 1;
+                    }
+
+                    if (i == 1) {
+                        mayor = calificacion;
+                        menor = calificacion;
+                    } else {
+                        if (calificacion > mayor) mayor = calificacion;
+                        if (calificacion < menor) menor = calificacion;
+                    }
+
+                    if (calificacion >= 6) {
+                        aprobadas++;
+                    } else {
+                        reprobadas++; 
+                }
+
+                    suma += calificacion; 
+
             }
-            cout << "Error: Edad invalida, favor de ingresar un numero entre 0 y 120." << endl;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        }    
 
-        while (true) {
-            cout << "Cuantas calificaciones desea registrar (1-3)? ";
-            if (cin >> n && n >= 1 && n <= 3) {
-                break;
+            promedio = suma / n;
+
+            cout << "RESUMEN DEL ESTUDIANTE" << endl;
+            cout << "Nombre: " << nombre << endl;
+            cout << "Edad: " << edad << endl;
+            cout << "Promedio: " << promedio << endl;
+            cout << "Calificacion mas alta: " << mayor << endl;
+            cout << "Calificacion mas baja: " << menor << endl;
+            cout << "Calificaciones aprobatorias: " << aprobadas << endl;
+            cout << "Calificaciones reprobatorias: " << reprobadas << endl;
+
+
+            cout << "Estado: ";
+            if (promedio >= 9.0) {
+                cout << "EXCELENTE" << endl;
+            } 
+            else if (promedio >= 7.0) {
+                cout << "APROBADO" << endl;
+            } 
+            else if (promedio >= 6.0) {
+                cout << "REGULAR" << endl;
+            } 
+            else {
+                cout << "REPROBADO" << endl;
             }
-            cout << "Error: Solo se pueden registrar de 1 a 3 calificaciones." << endl;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        
-        }
-        for (int i = 1; i <= n; i++) {
-        cout << "Ingrese la calificacion " << i << ": ";
-            cin >> calificacion;
-
-        if (calificacion < 0 || calificacion > 10) {
-            cout << "Error: Calificaciones invalidas." << endl;
-            return 1;
+            break;
         }
 
-        if (i == 1) {
-            mayor = calificacion;
-            menor = calificacion;
-        } else {
-            if (calificacion > mayor) mayor = calificacion;
-            if (calificacion < menor) menor = calificacion;
-        }
+        case 2:
+            cout << "Ver informacion del programa." << endl;
+            break;
 
-        if (calificacion >= 6) {
-            aprobadas++;
-        } else {
-            reprobadas++; 
-        }
+        case 3: 
+            cout << "Salir." << endl;
+            break;
 
-        suma += calificacion; 
-
-    }
-
-        promedio = suma / n;
-
-        cout << "RESUMEN DEL ESTUDIANTE" << endl;
-        cout << "Nombre: " << nombre << endl;
-        cout << "Edad: " << edad << endl;
-        cout << "Promedio: " << promedio << endl;
-        cout << "Calificacion mas alta: " << mayor << endl;
-        cout << "Calificacion mas baja: " << menor << endl;
-        cout << "Calificaciones aprobatorias: " << aprobadas << endl;
-        cout << "Calificaciones reprobatorias: " << reprobadas << endl;
-
-
-        cout << "Estado: ";
-        if (promedio >= 9.0) {
-            cout << "EXCELENTE" << endl;
+        default:
+            cout << "Error: Opcion invalida, solo elegir 1, 2 o 3." << endl;
+            break;
         } 
-        else if (promedio >= 7.0) {
-            cout << "APROBADO" << endl;
-        } 
-        else if (promedio >= 6.0) {
-            cout << "REGULAR" << endl;
-        } 
-        else {
-            cout << "REPROBADO" << endl;
-        }
-        break;
-    }
 
-    case 2:
-        cout << "Ver informacion del programa." << endl;
-        break;
-
-    case 3: 
-        cout << "Salir." << endl;
-        return 0;
-    } 
+    } while (opcion != 3);
 
     return 0;
 } 
